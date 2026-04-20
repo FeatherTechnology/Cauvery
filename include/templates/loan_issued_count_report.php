18546293b1f5ced330805cebc225986de426999e
Vasantharaj <vasanth3523av@gmail.com>
2026-04-20 12:30:55 +0530

    Changes & Corrections
    1. Developing Location Track Report
    2. In the reports where the user dropdown is available it is split into All, Active, and Inactive options
    3. Cleared Report add bank name dropdown

--- include/templates/loan_issued_count_report.php
+++ include/templates/loan_issued_count_report.php
@@ -9,7 +9,7 @@
 <br><br>
 <div class="page-header">
 	<div style="background-color:#0C70AB; width:100%; padding:12px; color: #ffff; font-size: 20px; border-radius:5px;">
-		Cauvery CApitals - Loan Issued Count Report
+		Cauvery Capitals - Loan Issued Count Report
 	</div>
 </div><br>
 <!-- Main container start -->
@@ -21,6 +21,12 @@
 			<div class="toggle-container col-12">
 				<input type="date" id='from_date' name='from_date' class="toggle-button" value=''>
 				<input type="date" id='to_date' name='to_date' class="toggle-button" value=''>
+				<select type="text" class="toggle-button" id='user_type' name='user_type'>
+                    <option value=''>Select User Type</option>
+                    <option value='1'>All</option>
+                    <option value='2'>Active</option>
+                    <option value='3'>In Active</option>
+                </select>
 				<select type="text" class="toggle-button" id='by_user' name='by_user'>
 					<option value=''>Select User</option>
 				</select>
