415e076ba4c5f1f60f4bb475baef91fe64debd81
akshaya <akshayaashok331@gmail.com>
2026-05-14 17:30:03 +0530

    Request
    Correction in agent and loan category - Completed

--- requestFile/getStaffBasedAgent.php
+++ requestFile/getStaffBasedAgent.php
@@ -8,16 +8,28 @@ $detailrecords = array();
 
 $result = $connect->query("SELECT agentforstaff FROM user where status = 0 and user_id = $user_id ");
 $row = $result->fetch();
-    $agentforstaff = $row['agentforstaff'];
 
-$agent_ids = explode(',', $agentforstaff);
-$i = 0;
-foreach ($agent_ids as $ag) {
-    $qry = $connect->query("SELECT ag_id, ag_name From agent_creation where ag_id = '" . $ag . "' ");
-    $row = $qry->fetch();
-    $detailrecords[$i]['ag_id'] = $row['ag_id'];
-    $detailrecords[$i]['ag_name'] = $row['ag_name'];
-    $i++;
+if ($row && !empty($row['agentforstaff'])) {
+
+    $agentforstaff = $row['agentforstaff'];
+    $agent_ids = explode(',', $agentforstaff);
+
+    $i = 0;
+
+    foreach ($agent_ids as $ag) {
+
+        if ($ag != '') {
+
+            $qry = $connect->query("SELECT ag_id, ag_name FROM agent_creation WHERE ag_id = '$ag'");
+            $agentRow = $qry->fetch();
+
+            if ($agentRow) {
+                $detailrecords[$i]['ag_id']   = $agentRow['ag_id'];
+                $detailrecords[$i]['ag_name'] = $agentRow['ag_name'];
+                $i++;
+            }
+        }
+    }
 }
 
 echo json_encode($detailrecords);
